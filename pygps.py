201ef4044c6de5c4b70c68e0a35d8f120199796b
iiirukaaa <iruka.mizutamaribond@gmail.com>
2025-09-08 12:23:33 +0900

    add some codes

--- pygps.py
+++ pygps.py
@@ -1,3 +1,17 @@
+from serial import Serial
 from micropyGPS import MicropyGPS
 
-gps = MicropyGPS()
+PORT = "/dev/serial0"
+BAUD = 9600
+
+gps = MicropyGPS(location_formatting='dd')
+
+with Serial(PORT, BAUD, timeout=1) as ser:
+    while True:
+        line = ser.readline()
+        if not line.startswith(b"$"):
+            continue
+        for ch in line.decode("ascii", errors="ignore"):
+            gps.update(ch)
+        print("lat/lon(dd):", gps.latitude[0], gps.longitude[0])
+        print("km/h:", gps.speed[2])
